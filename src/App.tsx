af2a372487f4f25b141382feed7b66f00da6d770
Bryan Cabrera Icte <bryanci2004@outlook.com>
2025-03-18 20:13:58 -0400

    Bryan Cabrera Icte

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,9 @@ function App() {
   }
   return (
     <div className="App">
+      <p>
+        Bryan Cabrera Icte: Hello World
+      </p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

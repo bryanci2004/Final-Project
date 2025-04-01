d322115ed803fed938c4d698d900d1826f95c821
Kush Patel <kushptl@udel.edu>
2025-04-01 16:38:04 -0400

    Containers created for basic and detailed question quizzes.

--- src/Home Page/HomePage.tsx
+++ src/Home Page/HomePage.tsx
@@ -12,15 +12,29 @@ function HomePage() {
             <h1>Home Page</h1>
         </header>
         <div className="HomePage-body">
+            <Container>
+                <Row>
+                    <h1> Welcome to Career Quiz! The first ever AI powered career quiz!</h1>
+                </Row>
+            </Container>
             <span>
                 <Container className="HomePage-questionsContainers">
                     <Row>
-                        <h2>Basic Questions</h2>
+                        <h2>Click here to take the Basic Questions quiz!</h2>
+                    </Row>
+                    <Row>
+                        <Button onClick={() => { navigate("/BasicQuestions"); } }>Basic Questions</Button>
+                    </Row>
+                </Container>
+                <Container className="HomePage-questionsContainers">
+                    <Row>
+                        <h2>Click here to take the Detailed Questions quiz! </h2>
+                    </Row>
+                    <Row>
+                        <Button onClick={() => { navigate("/DetailedQuestions"); } }>Detailed Questions</Button>
                     </Row>
                 </Container>
             </span>
-                <Button onClick={() => { navigate("/BasicQuestions"); } }>Basic Questions</Button>
-                <Button onClick={() => { navigate("/DetailedQuestions"); } }>Detailed Questions</Button>
                 <Button onClick={() => { navigate("/App"); } }>App</Button>
             </div>
         </>

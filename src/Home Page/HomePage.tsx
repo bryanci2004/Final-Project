71940ddf2ca50a526d71f6b84108e7946d5e0421
Kush Patel <kushptl@udel.edu>
2025-04-01 16:29:10 -0400

    Header started and made a css file for homepage.

--- src/Home Page/HomePage.tsx
+++ src/Home Page/HomePage.tsx
@@ -1,15 +1,28 @@
-import { Button } from "react-bootstrap";
+import { Button, Container, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import './HomePage.css'
 
 function HomePage() {
 
     const navigate = useNavigate();
 
     return (
-        <><h1>Home Page</h1>
-        <Button onClick={() => { navigate("/BasicQuestions"); } }>Basic Questions</Button>
-        <Button onClick={() => { navigate("/DetailedQuestions"); } }>Detailed Questions</Button>
-        <Button onClick={() => { navigate("/App"); } }>App</Button>
+        <>
+        <header className="HomePage-header">
+            <h1>Home Page</h1>
+        </header>
+        <div className="HomePage-body">
+            <span>
+                <Container className="HomePage-questionsContainers">
+                    <Row>
+                        <h2>Basic Questions</h2>
+                    </Row>
+                </Container>
+            </span>
+                <Button onClick={() => { navigate("/BasicQuestions"); } }>Basic Questions</Button>
+                <Button onClick={() => { navigate("/DetailedQuestions"); } }>Detailed Questions</Button>
+                <Button onClick={() => { navigate("/App"); } }>App</Button>
+            </div>
         </>
     );
 }

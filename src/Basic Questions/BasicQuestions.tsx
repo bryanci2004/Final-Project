66022e52b1538e1637402a577383c7731ffd9348
Bryan Cabrera Icte <bryanci2004@outlook.com>
2025-04-02 08:23:12 -0400

    A simple copy and paste of Detailed Question format to Basic Question. It works!

--- src/Basic Questions/BasicQuestions.tsx
+++ src/Basic Questions/BasicQuestions.tsx
@@ -1,12 +1,42 @@
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import './BasicQuestions.css';
 
 function BasicQuestions() {
     
     const navigate = useNavigate();
     return (
-        <><h1>Basic Questions</h1>
-        <Button onClick={() => { navigate("/"); } }>Home Page</Button></>
+        <>
+       <header className="BasicQuestions-header">
+                <h1>Detailed Questions</h1>
+                <Button onClick={() => { navigate("/"); }} className="BasicQuestions-homePageButton">Home Page</Button>
+            </header>
+        <div className="BasicQuestions-body">
+            <Container>
+                <Row className="justify-content-center mt-4">
+                    <Col md={5} className="BasicQuestions-questionsContainers">
+                        <h2>Question 2: Long-Term Career Goals</h2>
+                        <br></br>
+                        <h6>
+                        Do you enjoy driving to work? Yes or no?
+                        </h6>
+
+                        <Form className="d-flex justify-content-center align-items-center gap-3">
+                            <Form.Control 
+                                style={{ width: '57.5vw', height: '30vh' }} // Adjust width as needed
+                            />
+                        </Form>
+                        <span>
+                            <Button style={{margin: '40px', width: '100px'}}>Prev</Button>
+                            <Button style={{marginRight: '40px', width: '100px'}}>Next</Button>
+                        </span>
+                        
+                    </Col>
+                </Row>
+            </Container>
+
+        </div>
+        </>
     );
   }
   

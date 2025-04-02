58bb55f512f758cd1673f28145516d808a7a023d
Kush Patel <kushptl@udel.edu>
2025-04-02 00:02:00 -0400

    Added header

--- src/Detailed Questions/DetailedQuestions.tsx
+++ src/Detailed Questions/DetailedQuestions.tsx
@@ -1,12 +1,17 @@
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import './DetailedQuestions.css';
 
 function DetailedQuestions() {
     
     const navigate = useNavigate();
+
     return (
-        <><h1>Detailed Questions</h1>
-        <Button onClick={() => { navigate("/"); } }>Home Page</Button></>
+        <>
+        <header className="DetailedQuestions-header">
+            <h1>Detailed Questions</h1>
+        </header><Button onClick={() => { navigate("/"); } }>Home Page</Button>
+        </>
     );
   }
   
